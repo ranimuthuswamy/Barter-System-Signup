83f67052729e71a544ad50ecadc763f5a6595c9a
ranimuthuswamy <67144212+ranimuthuswamy@users.noreply.github.com>
2020-12-26 16:59:39 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,10 @@
+import * as React from "react";
+import SignupLoginScreen from "./screens/SignupLoginScreen";
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <SignupLoginScreen/>
+    );
+  }
+}
